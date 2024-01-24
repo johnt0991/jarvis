88ece5c7174d761c30f4b80d1812035f846e6a23
johnt0991 <johnt0991+github@gmail.com>
2024-01-24 09:35:36 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,14 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from flask import Flask
+
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
+app = Flask(__name__)
+
 client = slack.WebClient(token=os.environ['slack_token'])
 
-client.chat_postMessage(channel='#it-test-channel',text=response)   
+client.chat_postMessage(channel='#it-test-channel',text='This is a test message from Jarvis')
